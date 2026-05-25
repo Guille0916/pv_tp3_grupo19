51e574925659f3af00c92bc11613fb484210c168
Guille <achoguille@gmail.com>
2026-05-25 01:47:40 -0300

    Inicializar proyecto TP3 1) Instalacion de Vite y dependencias 2) Migracion de assets (img,css) del tp1 3) Creaccion e importacion del componente footer

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './assets/css/style.css'
+import Footer from './components/Footer.jsx'
+
+function App() {
+  return(
+   <>
+        <h1 style={{ color: 'blue', textAlign: 'center', marginTop: '50px' }}>
+         si ves esto react esta funcionando !!
+      </h1>
+   
+
+
+
+  <Footer/>
+  </>
+  );
+}
+
+export default App
