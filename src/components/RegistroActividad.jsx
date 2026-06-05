429583753d9b1dd806b28d7bee2f0bdb52784aea
lautarofernandez17 <laufernandez1704@gmail.com>
2026-06-05 18:43:33 -0300

    Correcion de RegistroActividad.jsx

--- src/components/RegistroActividad.jsx
+++ src/components/RegistroActividad.jsx
@@ -1,10 +1,11 @@
 import '../assets/static/styles/RegistroActividad.css';
+
 function RegistroActividad({ ultimaModificacion }) {
   return (
     <div className="contenedor-registro-actividad">
       <h5 className="contenedor-titulo-registro">Historial de Cambios</h5>
       <p className="texto-registro">
-        {ultimaModificacion ? ultimaModificacion : "Esperando modificaciones en la lista..."}
+        Última actualización de la lista: {ultimaModificacion} hs.
       </p>
     </div>
   );
