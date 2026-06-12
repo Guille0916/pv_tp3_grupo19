4124bf0553b1346c68d283bb7e2fd294d42f7ff4
AniQuintanilla13-debug <mikaelaanahi13@gmail.com>
2026-06-12 15:46:01 -0300

    Actividad 2, Vistas de Dashboard y Perfil de Usuario

--- src/views/PerfilUsuario.jsx
+++ src/views/PerfilUsuario.jsx
@@ -5,7 +5,7 @@ const PerfilUsuario = () => {
     <div className="card shadow-sm border-0 p-4 bg-white rounded animate__animated animate__fadeIn">
       <div className="card-body">
         <h2 className="card-title text-primary border-bottom pb-2 mb-4">
-          👤 Perfil de Usuario
+           Perfil de Usuario
         </h2>
         
         <div className="row g-3">
