4df109bef38ad57ae1c40c67edfd6224f3464c13
francozxy <thedarck777@gmail.com>
2026-06-12 10:51:28 -0300

    agregando route rreact  modificando  main  y app añadiendo nuevo jsx vacios pero con sus export  para la visualizacion del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './assets/static/styles/style.css'
 import Footer from './components/Footer.jsx'
 import Nav from './components/Nav.jsx'
 import Header from './components/Header.jsx'
-import ListaProyectos from './components/ListaProyectos.jsx'
 
+//importamos desde otra carpeta 
+import Dashboard from './views/Dashboard.jsx'
+import ListaProyectos from './views/ListaProyectos.jsx'
+import DetalleProyecto from './views/DetalleProyecto.jsx'
+import PerfilUsuario from './views/PerfilUsuario.jsx'
 function App() {
   return (
     <div className="app">
@@ -12,7 +17,16 @@ function App() {
         <Header></Header>
         <Nav></Nav>
       </div>
-      <ListaProyectos></ListaProyectos>
+      <main> 
+        <Routes>
+          <Route path="/" element={<Navigate to="dashboard" replace />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="lista-proyectos" element={<ListaProyectos/>} />
+          <Route path="proyecto/:id" element={<DetalleProyecto />} />
+          <Route path="perfil" element={<PerfilUsuario />} />
+        </Routes>
+      </main>
+
       <Footer></Footer>
 
     </div>
---
+++ src/views/PerfilUsuario.jsx
@@ -0,0 +1,13 @@
+// src/views/PerfilUsuario.jsx
+
+function PerfilUsuario() {
+  return (
+    <div>
+      {/* Contenido de tu perfil de usuario */}
+      <h2>Perfil de Usuario</h2>
+    </div>
+  );
+}
+
+// ¡Asegúrate de agregar esta línea al final!
+export default PerfilUsuario;
