9e07d2fc3ec239940f7c60c295855b1efdafdfba
Guille <achoguille@gmail.com>
2026-06-17 19:35:21 -0300

    modificacion del header para hacerlo dinamico con bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import '../assets/static/styles/Header.css';
 import logoEducacion from '../assets/img/logo-educacion.png';
-
+import { useContext } from 'react';
+import { UsuarioContext } from '../context/UsuarioContext';
 const Header = () => {
+  const { usuario } = useContext(UsuarioContext);
   return (
         <header className="header-contenedor">
           <div className="header-logo">
@@ -15,6 +17,10 @@ const Header = () => {
       />
       <h1>Gestion de Proyectos Educativos</h1>
     </div>
+    <div className="usuario-info text-end text-dark p-2 bg-light rounded shadow-sm">
+          <p className="mb-0 fw-bold">{usuario.nombre}</p>
+          <span className="badge bg-primary">{usuario.rol}</span>
+        </div>
     </header>
   );
 };
